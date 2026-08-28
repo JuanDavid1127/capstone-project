1243c132756588c6730c10f780c8dd0e7825573a
JuanDavid1127 <davidjuancarlos1127@gmail.com>
2026-08-29 01:45:33 +0800

    added function to display data to dashboard

--- frontend/src/js/dashboard.js
+++ frontend/src/js/dashboard.js
@@ -1,14 +1,39 @@
-checkAuth();
+const token = localStorage.getItem("token");
+const available = document.querySelector("#available");
+checkAuth(token);
+loadStudents(token);
 
 window.addEventListener("pageshow", (event) => {
         if(event.persisted) {
-            checkAuth();
+            checkAuth(token);
     }
 })
 
-function checkAuth() {
-    const token = localStorage.getItem("token");
+function checkAuth(token) {
     if(!token) {
         window.location.href = "../../index.html";
     }
+}
+
+function loadStudents(token) {
+    available.innerHTML = "";
+    fetch("http://localhost:3000/students?grade_level=G10", {
+        headers: {
+            "Authorization" : `Bearer ${token}`
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(student => {
+            const row = `<tr>
+                            <td><b>${student.name}</b></td>
+                            <td>${student.grade_level}</td>
+                            <td>${student.lrn}</td>
+                            <td><button class="assign">Assign</button></td>
+                        </tr>
+                        `
+            available.innerHTML += row;
+        })
+        
+    });
 }
